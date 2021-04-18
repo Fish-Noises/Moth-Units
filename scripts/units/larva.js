3d5ab6a35a47af426bf5d776654816c9e602114a
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-18 10:57:02 +0930

    Update larva.js

--- scripts/units/larva.js
+++ scripts/units/larva.js
@@ -2,5 +2,4 @@
 let col = Color.valueOf("#bf8af4");
 const MothAirT1 = extend(UnitType, "larva",{});
 MothAirT1.constructor = () => extend(UnitEntity, {});
-MothAirT1.defaultController = DefenderAI;
 MothAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
