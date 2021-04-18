3197c9bbd2e6e89b8c49a6775dafe62575a525f2
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-18 11:49:23 +0930

    Update larva.js

--- scripts/units/larva.js
+++ scripts/units/larva.js
@@ -1,5 +1,5 @@
 //lmao pirated from missing category units
-let col = Color.valueOf("#bf8af4");
-const MothAirT1 = extend(UnitType, "larva",{});
-MothAirT1.constructor = () => extend(UnitEntity, {});
-MothAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
+//let col = Color.valueOf("#bf8af4");
+//const MothAirT1 = extend(UnitType, "larva",{});
+//MothAirT1.constructor = () => extend(UnitEntity, {});
+//MothAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
