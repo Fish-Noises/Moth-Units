db0f13dd886a5fd320e5d271756412361e62fdf8
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-18 10:02:22 +0930

    Create main.js

---
+++ scripts/units/main.js
@@ -0,0 +1,67 @@
+//mmm yes code stolen from Project Unity
+global.mcu = {};
+const loadFile = (prev, array) => {
+  var results = [];
+  var names = [];
+
+  var p = prev;
+
+  for(var i = 0; i < array.length; i++){
+    var file = array[i];
+
+    if(typeof(file) === "object"){
+      p.push(file.name);
+      var temp = loadFile(p, file.childs);
+
+      results = results.concat(temp.res);
+      names = names.concat(temp.fileNames);
+
+      p.pop();
+    }else{
+      var temp = p.join("/") + "/" + file;
+
+      results.push(temp);
+      names.push(file);
+    };
+  };
+
+  return {
+    res: results,
+    fileNames: names
+  };
+};
+
+//Basically just folders and the stuff inside those folders.
+const script = [
+  {
+    name: "units",
+    childs: [["needle", "dart", "spear", "javelin", "harpoon"]]
+  },
+  {
+    name: "loader",
+    childs: ["unitLoader"] //load after everything, make sure it exists.
+  }
+];
+
+const loadedScript = loadFile([], script);
+for(var i = 0; i < loadedScript.res.length; i++){
+  var res = loadedScript.res[i];
+  var name = loadedScript.fileNames[i];
+  try{
+    var content = require("moth-units/" + res);
+    if(typeof(content) !== "undefined"){
+      global.mcu[name] = content;
+    };
+  }catch(e){
+    print(e);
+	};
+};
+
+if(!Vars.headless){
+  Core.app.post(() => {
+    const meta = Vars.mods.locateMod("moth-units").meta;
+    meta.displayName = "Moth's units"
+    meta.author = "Fish-Noises, mostly pirated from missing-category-units";
+    meta.description = "No";
+  });
+}
