b5e40abbe85a702619f4c86e854f138001ffe105
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-18 10:26:27 +0930

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ const loadFile = (prev, array) => {
 const script = [
   {
     name: "units",
-    childs: ["needle", "dart", "spear", "javelin", "harpoon"]
+    childs: ["larva", "hawk", "luna", "atlas", "death-head"]
   },
   {
     name: "loader",
