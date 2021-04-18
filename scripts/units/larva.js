0138863a422b0a8aad9e1469451b268e1da50faa
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-18 10:23:07 +0930

    Update larva.js

--- scripts/units/larva.js
+++ scripts/units/larva.js
@@ -1,5 +1,6 @@
 //lmao pirated from missing category units
 let col = Color.valueOf("#bf8af4");
 const MothAirT1 = extend(UnitType, "larva",{});
+MothAirt1.constructor = () => extend(UnitEntity, {});
 MothAirT1.defaultController = ais.DefenderAI;
-SpAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
+MothAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
