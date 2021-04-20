1adbddd5ec3da622faf8a069d79ae174c960c710
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-04-20 13:34:37 +0930

    Update and rename larva.js to pupa.js

--- scripts/units/moth-units/pupa.js
+++ scripts/units/moth-units/pupa.js
@@ -1,5 +1,5 @@
 //lmao pirated from missing category units
 let col = Color.valueOf("#bf8af4");
-const MothAirT1 = extend(UnitType, "larva",{});
+const MothAirT1 = extend(UnitType, "pupa",{});
 MothAirT1.constructor = () => extend(UnitEntity, {});
 MothAirT1.abilities.add(new ForceFieldAbility(60f, 0.3f, 400f, 60f * 6));
