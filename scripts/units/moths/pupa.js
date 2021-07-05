46535965d89999f2bbc1056cab65113eb002a634
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-07-05 18:33:59 +0930

    Update pupa.js

--- scripts/units/moths/pupa.js
+++ scripts/units/moths/pupa.js
@@ -3,4 +3,4 @@ let col = Color.valueOf("#bf8af4");
 const MothAirT1 = extend(UnitType, "pupa",{});
 MothAirT1.constructor = () => extend(UnitEntity, {});
 MothAirT1.abilities.add(new ForceFieldAbility(60, 0.3, 400, 60 * 6));
-MothAirT1.defaultController = defenderAI
+MothAirT1.defaultController = defenderAI;
