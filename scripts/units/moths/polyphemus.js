659e15854f593fe16aba5f92ff4bf13669fc1321
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-11-21 13:03:37 +1030

    Update polyphemus.js

--- scripts/units/moths/polyphemus.js
+++ scripts/units/moths/polyphemus.js
@@ -4,19 +4,19 @@ let col = Color.valueOf("#bf8af4");
 const MothAirT4 = extend(UnitType, "polyphemus",{});
 MothAirT4.constructor = () => extend(UnitEntity, {});
 
-const polyphemusRay1 = extend(Weapon, "overload-ray", {
+const polyphemusRay1 = extend(Weapon, "moth-units-overload-ray", {
   x: 9,
   y: 19,
   rotate: true
 });
 
-const polyphemusRay2 = extend(Weapon, "overload-ray", {
+const polyphemusRay2 = extend(Weapon, "moth-units-overload-ray", {
   x: 20,
   y: 16,
   rotate: true
 });
 
-const polyphemusRay3 = extend(Weapon, "overload-ray", {
+const polyphemusRay3 = extend(Weapon, "moth-units-overload-ray", {
   x: 13,
   y: -1,
   rotate: true
