a13ef475d663178b429d11e020497caf31551213
Fish-Noises <76248882+Fish-Noises@users.noreply.github.com>
2021-11-21 19:59:38 +1030

    Update polyphemus.js

--- scripts/units/moths/polyphemus.js
+++ scripts/units/moths/polyphemus.js
@@ -10,38 +10,38 @@ const polyphemusRay1 = extend(Weapon, "moth-units-overload_ray", {
   y: 19,
   rotate: true,
   shootSound: Sounds.tractorbeam,
-  reload: 1,
+  reload: 30,
   continuous: true,
   alternate: false,
   shootStatusDuration: 15,
   shootStatus: StatusEffects.unmoving,
-  bullet: bullets.newPrismBeam(25,210,45)
+  bullet: bullets.newPrismBeam(25,210,240)
 });
 
 const polyphemusRay2 = extend(Weapon, "moth-units-overload_ray", {
   x: 20,
   y: 16,
   rotate: true,
-  reload: 15,
+  reload: 30,
   shootSound: Sounds.tractorbeam,
   continuous: true,
   alternate: false,
   shootStatusDuration: 30,
   shootStatus: StatusEffects.unmoving,
-  bullet: bullets.newPrismBeam(50,210,45)
+  bullet: bullets.newPrismBeam(50,210,240)
 });
 
 const polyphemusRay3 = extend(Weapon, "moth-units-overload_ray", {
   x: 13,
   y: -1,
   rotate: true,
-  reload: 15,
+  reload: 30,
   shootSound: Sounds.tractorbeam,
   continuous: true,
   alternate: false,
   shootStatusDuration: 30,
   shootStatus: StatusEffects.unmoving,
-  bullet: bullets.newPrismBeam(125,210,45)
+  bullet: bullets.newPrismBeam(125,210,240)
 });
 
 MothAirT4.weapons.addAll(polyphemusRay1,polyphemusRay2,polyphemusRay3);
